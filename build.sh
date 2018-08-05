a3b126ca07247002073c688e2220deeb98ca05e0
MrPoundsign <mrpoundsign@gmail.com>
2018-08-05 00:47:24 -0700

    Add cross compile to build

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 HERE=`dirname $0`
-mkdir -p $HERE/bin
-go build -ldflags="-s -w" -o bin/poundbot poundbot.go
-upx bin/poundbot
+cd $HERE
+BUILD_DIR=$HERE/builds
+mkdir -p $BUILD_DIR
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR/linux
+mkdir -p $BUILD_DIR/darwin
+mkdir -p $BUILD_DIR/windows
+env GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o $BUILD_DIR/windows/poundbot.exe poundbot.go
+env GOOS=darwin GOARCH=amd64 go build -o $BUILD_DIR/dawrin/poundbot poundbot.go
+env GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o $BUILD_DIR/linux/poundbot poundbot.go
+upx $BUILD_DIR/linux/poundbot
