a9e4dedef641853a310a646ae6cabf0e802cba37
Mister Poundsign <mrpoundsign@gmail.com>
2018-08-06 18:34:16 -0700

    Read PoundBotConnector version directly from plugin

--- package.sh
+++ package.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 HERE=`dirname \`readlink -f $0\``
 cd $HERE
+VERSION=`cat VERSION`
+PLUGIN_VERSION=`egrep "\[Info\(" rust_plugin/PoundbotConnector.cs | cut -d \" -f 6`
+echo "Packaging v${VERSION}, Plugin v${PLUGIN_VERSION}"
 if [ ! -d builds ]; then
     echo "Builds not found. Run build.sh first."
     exit 1
@@ -9,8 +12,10 @@ fi
 for x in windows linux darwin
 do
     cd $HERE/builds/$x
-    zip -9r PoundBot-`[[ $x = "darwin" ]] && echo "OSX" || echo $x`-`cat $HERE/VERSION`.zip .
+    FILE=PoundBot-`[[ $x = "darwin" ]] && echo "OSX" || echo $x`-${VERSION}.zip
+    echo "Creating ${FILE}"
+    zip -9r PoundBot-`[[ $x = "darwin" ]] && echo "OSX" || echo $x`-${VERSION}.zip .
 done
 cd $HERE/builds
 cp $HERE/rust_plugin/PoundbotConnector.cs .
-zip -9 PoundbotConnector-Plugin-`cat $HERE/VERSION`.zip PoundbotConnector.cs
+zip -9 PoundbotConnector-Plugin-${PLUGIN_VERSION}.zip PoundbotConnector.cs
