e2ac0e020bc0705701f13e79dc26581bade2e46d
Mister Poundsign <mrpoundsign@gmail.com>
2019-01-09 12:06:21 -0800

    Move poundbot cmd into it's own space

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,10 +3,10 @@ source `dirname $0`/env.sh
 
 rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR/linux
-mkdir -p $BUILD_DIR/darwin
-mkdir -p $BUILD_DIR/windows
+# mkdir -p $BUILD_DIR/darwin
+# mkdir -p $BUILD_DIR/windows
 
-env GOOS=windows GOARCH=amd64 go build -ldflags="$LDFLAGS" -o $BUILD_DIR/windows/poundbot.exe $MAIN/cmd/poundbot.go
-env GOOS=darwin GOARCH=amd64 go build -ldflags="$LDFLAGS" -o $BUILD_DIR/darwin/poundbot $MAIN/cmd/poundbot.go
-env GOOS=linux GOARCH=amd64 go build -ldflags="$LDFLAGS" -o $BUILD_DIR/linux/poundbot $MAIN/cmd/poundbot.go
+# env GOOS=windows GOARCH=amd64 go build -ldflags="$LDFLAGS" -o $BUILD_DIR/windows/poundbot.exe $MAIN/cmd/poundbot/poundbot.go
+# env GOOS=darwin GOARCH=amd64 go build -ldflags="$LDFLAGS" -o $BUILD_DIR/darwin/poundbot $MAIN/cmd/poundbot/poundbot.go
+env GOOS=linux GOARCH=amd64 go build -ldflags="$PB_LDFLAGS" -o $BUILD_DIR/linux/poundbot $MAIN/cmd/poundbot/poundbot.go
 upx $BUILD_DIR/linux/poundbot
--- scripts/env.sh
+++ scripts/env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 MAIN=`realpath "$(dirname $0)/.."`
 BUILD_DIR=$MAIN/builds
-VERSION=`cat $MAIN/VERSION`
-LDFLAGS="-s -w -X main.version=$VERSION -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.githash=`git rev-parse --short HEAD`"
+PB_VERSION=`cat $MAIN/cmd/poundbot/VERSION`
+PB_LDFLAGS="-s -w -X main.version=$PB_VERSION -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.githash=`git rev-parse --short HEAD`"
+PBWEB_VERSION=`cat $MAIN/cmd/pbweb/VERSION`
+PBWEB_LDFLAGS="-s -w -X main.version=$PBWEB_VERSION -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.githash=`git rev-parse --short HEAD`"
 PLUGIN_VERSION=`egrep "\[Info\(" $MAIN/rust_plugin/PoundBotConnector.cs | cut -d \" -f 6`
