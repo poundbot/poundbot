77605ffff5eaf5f89c8c6cf432996ed377fee4bc
Mister Poundsign <mrpoundsign@gmail.com>
2019-04-13 14:48:27 -0700

    Fix some logging messages. Bump version.

--- rustconn/clan.go
+++ rustconn/clan.go
@@ -26,19 +26,27 @@ func NewClan(logPrefix string, as storage.AccountsStore, us storage.UsersStore) 
 // Handle manages individual clan REST requests form the Rust server
 func (c *clan) Handle(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
-	// account := context.Get(r, contextKeyAccount).(types.Account)
+	account := r.Context().Value(contextKeyAccount).(types.Account)
 	serverKey := r.Context().Value(contextKeyServerKey).(string)
+	requestUUID := r.Context().Value(contextKeyRequestUUID).(string)
+	server, err := account.ServerFromKey(serverKey)
+	if err != nil {
+		handleError(w, types.RESTError{
+			StatusCode: http.StatusInternalServerError,
+			Error:      "Internal error",
+		})
+	}
 
 	vars := mux.Vars(r)
 	tag := vars["tag"]
 
 	switch r.Method {
 	case http.MethodDelete:
-		c.logger.Printf("clanHandler: Removing clan %s for %s\n", tag, serverKey)
+		c.logger.Printf("[%s] Removing clan %s for %s:%s\n", requestUUID, tag, account.ID, server.Name)
 		c.as.RemoveClan(serverKey, tag)
 		return
 	case http.MethodPut:
-		c.logger.Printf("clanHandler: Updating clan %s for %s\n", tag, serverKey)
+		c.logger.Printf("[%s] Updating clan %s for %s:%s\n", requestUUID, tag, account.ID, server.Name)
 		decoder := json.NewDecoder(r.Body)
 		var t types.ServerClan
 		err := decoder.Decode(&t)
