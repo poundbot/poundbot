b04e2c75cd09d009d2a90484987b0419e80c04ef
MrPoundsign <mrpoundsign@gmail.com>
2019-05-11 14:28:57 -0700

    New build scripts

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 MAIN=`realpath "$(dirname $0)/.."`
 BUILD_DIR=$MAIN/builds
+LANGUAGE_DIR=$MAIN/language
+TEMPLATES_DIR=$MAIN/templates_sample
 PB_VERSION=`cat $MAIN/cmd/poundbot/VERSION`
 PB_LDFLAGS="-s -w -X main.version=$PB_VERSION -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.githash=`git rev-parse --short HEAD`"
 PBWEB_VERSION=`cat $MAIN/cmd/pbweb/VERSION`
