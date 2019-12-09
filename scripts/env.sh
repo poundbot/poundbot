5e30c6d31aa366073ecb377a6e624c3062f75dea
MrPoundsign <mrpoundsign@gmail.com>
2019-12-08 17:15:27 -0800

    Remove junk from build script

--- scripts/env.sh
+++ scripts/env.sh
@@ -5,6 +5,3 @@ LANGUAGE_DIR=$MAIN/language
 TEMPLATES_DIR=$MAIN/templates_sample
 PB_VERSION=`cat $MAIN/cmd/poundbot/VERSION`
 PB_LDFLAGS="-s -w -X main.version=$PB_VERSION -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.githash=`git rev-parse --short HEAD`"
-PBWEB_VERSION=`cat $MAIN/cmd/pbweb/VERSION`
-PBWEB_LDFLAGS="-s -w -X main.version=$PBWEB_VERSION -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.githash=`git rev-parse --short HEAD`"
-PLUGIN_VERSION=`egrep "\[Info\(" $MAIN/rust_plugin/PoundBotConnector.cs | cut -d \" -f 6`
